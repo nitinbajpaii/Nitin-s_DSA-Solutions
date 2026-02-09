52f83e538ac18cd831fdc5b8096d3160d8b0ed22
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-02-09 18:45:06 +0530

    Time: 4 ms (98.41%), Space: 46.1 MB (45.24%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        for(char ch:moves.toCharArray()){
+            switch(ch){
+             case 'U' : x++; break;
+             case 'D' : x--; break;
+             case 'R' : y++; break;
+             case 'L' : y--; break;
+            }
+            
+        }
+        return x==0 && y==0;
+    }
+}
