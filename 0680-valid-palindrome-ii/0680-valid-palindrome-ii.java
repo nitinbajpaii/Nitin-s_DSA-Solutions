4f06d3355c6c68af6be6f12bdd955aec14a88c32
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-02-11 12:20:53 +0530

    Time: 4 ms (99.48%), Space: 47.8 MB (66.4%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j= s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return isPalli(s,i+1,j) || isPalli(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+    public boolean isPalli(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+}
