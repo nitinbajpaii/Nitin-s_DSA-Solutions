ced16c03bf5266c99b1cef889fd13520a8abdfc4
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-02-21 12:37:36 +0530

    Time: 3 ms (93.01%), Space: 44.7 MB (94.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -6,16 +6,15 @@ class Solution {
         for(int i=0;i<n;i++){
             int curr = nums1[i];
             int ans =-1;
-
             for(int j=0;j<m;j++){
                 if(nums2[j]==curr){
                     for(int k=j+1;k<m;k++){
                         if(nums2[k]>curr){
-                            ans = nums2[k];
+                            ans=nums2[k];
                             break;
                         }
                     }
-
+                    break;
                 }
             }
             res[i]=ans;
