302ce055a925b9665c655dccef578ef3218b0aa7
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-02-26 19:07:01 +0530

    Time: 4 ms (76.76%), Space: 43.6 MB (42.16%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count=0;
+        int n = heights.length;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=heights[i];
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            if(arr[i]!=heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
