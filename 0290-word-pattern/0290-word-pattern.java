17a42f0444d8308eb9d9d7f5bbe615e7bc4b9dc8
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-03-14 11:00:48 +0530

    Time: 2 ms (16.6%), Space: 43.2 MB (15.6%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character,String> map = new HashMap<>();
+        String[] arr = s.trim().split("\\s+");
+        if(pattern.length()!=arr.length){
+            return false;
+        }
+        for(int i=0;i<pattern.length();i++){
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(arr[i])){
+                    return false;
+                }
+            }
+            else{
+                if(map.containsValue(arr[i])){
+                    return false;
+                }
+            }
+            map.put(ch,arr[i]);
+        }
+        return true;
+    }
+}
