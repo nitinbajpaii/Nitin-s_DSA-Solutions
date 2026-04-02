9135141a8b621c928367c77f0d7942c3b22367cb
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-04-02 12:17:58 +0530

    Time: 0 ms (100%), Space: 41.4 MB (99.6%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,25 +1,33 @@
 class Solution {
     public int findComplement(int n) {
-        String rev ="";
-        while(n>0){
-            if(n%2==1){
-                rev += '1';
-            }
-            else{
-                rev += '0';
-            }
-            n /= 2;
+        //Method -1
+        // String rev ="";
+        // while(n>0){
+        //     if(n%2==1){
+        //         rev += '1';
+        //     }
+        //     else{
+        //         rev += '0';
+        //     }
+        //     n /= 2;
+        // }
+        // String binary = new StringBuilder(rev).reverse().toString();
+        // String ans ="";
+        // for(int i=0;i<binary.length();i++){
+        //     if(binary.charAt(i)=='1'){
+        //         ans +='0';
+        //     }
+        //     else{
+        //         ans += '1';
+        //     }
+        // }
+        // return Integer.parseInt(ans,2);
+
+        //Method -2
+        int mask =1;
+        while(mask<n){
+            mask = mask<<1|1;
         }
-        String binary = new StringBuilder(rev).reverse().toString();
-        String ans ="";
-        for(int i=0;i<binary.length();i++){
-            if(binary.charAt(i)=='1'){
-                ans +='0';
-            }
-            else{
-                ans += '1';
-            }
-        }
-        return Integer.parseInt(ans,2);
+        return mask^n;
     }
 }
