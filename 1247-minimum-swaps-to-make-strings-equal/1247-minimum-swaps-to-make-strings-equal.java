24fdbe04f261f2b3f8c0040ae8a68c8f0df29d6d
Nitin Bajpai <nitinbajpai2006@gmail.com>
2026-08-25 11:15:50 +0530

    Time: 1 ms (92.92%), Space: 42.6 MB (92.63%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumSwap(String s1, String s2) {
+        int xy =0;
+        int yx =0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)==s2.charAt(i)){
+                continue;
+            }
+            if(s1.charAt(i)=='x'){
+                xy++;
+            }
+            else{
+                yx++;
+            }
+        }
+        if((xy+yx) %2!=0){
+            return -1;
+        }
+        return xy/2 + yx/2 + (xy%2) *2;
+    }
+}
